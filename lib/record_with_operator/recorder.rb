86327043e159daf0657346839a192095cd653dd1
Nate Mortensen <nate@clearvoice.com>
2017-03-22 13:10:49 -0600

    Add options to provide custom operations

--- lib/record_with_operator/recorder.rb
+++ lib/record_with_operator/recorder.rb
@@ -24,12 +24,8 @@ module RecordWithOperator
     end
 
     module ClassMethods
-      VALID_ACTIONS = %w(create update destroy)
-
-      def records_with_operator_on(*args)
-        raise "valid actions are #{VALID_ACTIONS.inspect}." unless args.all?{|arg| VALID_ACTIONS.include?(arg.to_s) }
-
-        @records_on = args.map(&:to_s)
+      def records_with_operator_on(*args, **custom_operations)
+        @records_on = (args + custom_operations.keys).map(&:to_s)
 
         before_create :set_creator if records_creator?
         before_save :set_updater if records_updater?
@@ -39,6 +35,16 @@ module RecordWithOperator
           belongs_to :creator, {:foreign_key => "created_by", :class_name => RecordWithOperator.config[:operator_class_name]}.merge(RecordWithOperator.config[:operator_association_options]) if records_creator?
           belongs_to :updater, {:foreign_key => "updated_by", :class_name => RecordWithOperator.config[:operator_class_name]}.merge(RecordWithOperator.config[:operator_association_options]) if records_updater?
           belongs_to :deleter, {:foreign_key => "deleted_by", :class_name => RecordWithOperator.config[:operator_class_name]}.merge(RecordWithOperator.config[:operator_association_options]) if records_deleter?
+
+          custom_operations.each do |operator, column|
+            define_method "set_#{operator}" do
+              return unless send("#{column}_changed?") && operator.present?
+              send("#{column}=", operator.id)
+            end
+
+            belongs_to operator, {foreign_key: operator.sub(/\wr$/, 'ed_by'), class_name: RecordWithOperator.config[:operator_class_name]}.merge(RecordWithOperator.config[:operator_association_options])
+            before_save "set_#{operator}"
+          end
         end
       end
 
